b059a783bd3b194fbed2676c8f58691952768bdd
arxarx123 <info@yourgurunet.com>
2021-09-16 21:08:35 +0300

    Created home screen

--- App.js
+++ App.js
@@ -1,23 +1,8 @@
-import React, {useEffect} from 'react';
-import {SafeAreaView, ScrollView, StatusBar, View, Text} from 'react-native';
-
-import io from 'socket.io-client';
+import React from 'react';
+import HomeScreen from './screen/HomeScreen';
 
 const App = () => {
-  useEffect(() => {
-    io('http://192.168.8.233:3000');
-  }, []);
-
-  return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView>
-        <View>
-          <Text>Chat app</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <HomeScreen />;
 };
 
 export default App;
